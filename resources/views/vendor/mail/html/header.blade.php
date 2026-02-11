887c4d703c33b5af4be9877b602674aaf721757e
Wishoje <kovacevic.milan88@gmail.com>
2026-02-11 13:12:56 -0600

    fix logo mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
-<td class="header">
+<td class="header" style="padding: 24px 0 14px;">
 <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
-<img src="{{ url('/marketing/gexOptionsLogo.png') }}" class="logo" alt="GexOptions" style="max-height: 44px; width: auto;">
+<img src="{{ url('/marketing/gexOptionsLogo.png') }}" alt="GexOptions" width="180" style="display:block; width:180px; max-width:100%; height:auto; border:0;">
 </a>
 </td>
 </tr>
